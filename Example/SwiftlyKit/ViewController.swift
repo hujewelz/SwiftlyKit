b51489ec8bf2a6d138ab6cf3b305ab1f6a3d6c99
huluobo <hujewelz@163.com>
2019-03-30 09:13:14 +0800

    Initial commit

---
+++ Example/SwiftlyKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftlyKit
+//
+//  Created by hujewelz on 03/30/2019.
+//  Copyright (c) 2019 hujewelz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
